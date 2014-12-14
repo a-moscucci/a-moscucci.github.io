38a00bddd7dcd60b78678ccc00a6c7c8b91668cd
a-moscucci <mauromoscucci@Mauros-MacBook-Air.local>
2014-12-13 21:41:05 -0500

    all new files

---
+++ js/settings.js
@@ -0,0 +1,12 @@
+  //Settings for kwicks
+  $().ready(function() {
+    $('.kwicks').kwicks({
+      maxSize : 200,
+      behavior: 'menu'
+    });
+  });
+
+//settings for lightgallery
+  $(document).ready(function() {
+    $("#lightGallery").lightGallery();
+  });
